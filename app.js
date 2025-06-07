a7154c755473fbc307efa8726fddcf662cb3a57d
MilanAnilAnthore <157392523+MilanAnilAnthore@users.noreply.github.com>
2025-06-07 17:18:34 -0400

    HelioScope Start
    This commit is the start of my project helioscope

---
+++ app.js
@@ -0,0 +1,83 @@
+const body = document.querySelector('body');
+const themeMode = document.querySelector('.themeMode');
+const pickerContainer = document.querySelector('.pickerContainer');
+const fromDate = document.querySelector('#fromDate');
+const endDate = document.querySelector('#endDate');
+const pickerSearch = document.querySelector('.pickerSearch');
+
+
+// Variables
+const api = "2RptH3VHc9l03KbmaL2iY9sws37rdlfNyburl0u2";
+let retrievedFromDate;
+let retrievedEndDate;
+
+
+function initializeDate() {
+
+    const today = new Date();
+    const thirtyDaysAgo = new Date();
+
+    thirtyDaysAgo.setUTCDate(today.getUTCDate() - 30);
+
+    function formatDate(date) {
+        const year = date.getUTCFullYear();
+        const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+        const day = String(date.getUTCDate()).padStart(2, '0');
+        return `${year}-${month}-${day}`
+    }
+
+    const defaultEndDate = formatDate(today);
+    const defaultStartDate = formatDate(thirtyDaysAgo);
+
+    fromDate.value = defaultStartDate;
+    endDate.value = defaultEndDate;
+
+}
+
+initializeDate();
+
+
+
+pickerSearch.addEventListener('click', () => {
+
+    if (endDate.value > fromDate.value) {
+
+        retrievedFromDate = fromDate.value;
+        retrievedEndDate = endDate.value;
+
+        getData(retrievedFromDate, retrievedEndDate);
+    } else {
+        alert('The from date should always be  than end date')
+    }
+
+})
+
+
+
+
+async function getData(startDate, toDate) {
+    try {
+        const response = await fetch(`https://api.nasa.gov/DONKI/FLR?startDate=${startDate}&endDate=${toDate}&api_key=${api}`)
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+        } else {
+            throw new Error('Failed to fetch data');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+
+
+
+// // This is for the toggling of theme
+// themeMode.addEventListener('click', () => {
+//     body.classList.toggle('white')
+// })
+
+
+
+
+
