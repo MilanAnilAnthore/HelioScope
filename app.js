86b7c673d9e1b8c1474b133b150ee90f21cddd79
MilanAnilAnthore <157392523+MilanAnilAnthore@users.noreply.github.com>
2025-06-25 16:32:00 -0400

    durationCalculation
    Found a way to convert the UTC time to millisecond which made it possible to compare different time and later converted the milliseconds to respective human readable formats

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const pickerSearch = document.querySelector('.pickerSearch');
 
 // Variables
 const api = "2RptH3VHc9l03KbmaL2iY9sws37rdlfNyburl0u2";
-let retrievedFromDate;
-let retrievedEndDate;
 let data;
 
 
@@ -43,8 +41,8 @@ pickerSearch.addEventListener('click', () => {
 
     if (endDate.value > fromDate.value) {
 
-        retrievedFromDate = fromDate.value;
-        retrievedEndDate = endDate.value;
+        let retrievedFromDate = fromDate.value;
+        let retrievedEndDate = endDate.value;
 
         getData(retrievedFromDate, retrievedEndDate);
     } else {
@@ -71,9 +69,91 @@ async function getData(startDate, toDate) {
 }
 
 
-function allData() {
-    let length = data.length;
+let result = [];
+let highestPeakTimeMinutes = null;
+let highestPeakTime;
+let duration;
 
+function allData(data) {
+    result = [];
+    for (let d of data) {
+        if (d.peakTime) {
+            const p = new Date(d.peakTime);
+            let minutes = p.getTime();
+            if (minutes > highestPeakTimeMinutes) {
+                highestPeakTimeMinutes = minutes;
+                highestPeakTime = d.peakTime.slice(11, 16);
+            }
+        }
+
+        if (d.endTime == null) {
+            let bt = new Date(d.beginTime);
+            let pt = new Date(d.peakTime);
+            let btMin = bt.getTime();
+            let ptMin = pt.getTime();
+            if (ptMin > btMin) {
+                let difference = ptMin - btMin;
+                duration = milliSecConverter(difference);
+            } else {
+                duration = "Unknown";
+            }
+        } else if (d.endTime) {
+            let bt = new Date(d.beginTime);
+            let et = new Date(d.endTime);
+            let btMin = bt.getTime();
+            let etMin = et.getTime();
+            if (etMin > btMin) {
+                let difference = etMin - btMin;
+                duration = milliSecConverter(difference);
+            } else {
+                duration = "Unknown";
+            }
+        }
+
+        let beginTime = d.beginTime ? d.beginTime.slice(11, 16) : null;
+        let peakTime = d.peakTime ? d.peakTime.slice(11, 16) : null;
+        let endTime = d.endTime ? d.endTime.slice(11, 16) : null;
+        let intensity = d.classType ? d.classType.slice(0, 1) : null;
+        let magnitude = d.classType ? d.classType.slice(1, 4) : null;
+        let instrumentUsed = d.instruments && d.instruments[0] ? d.instruments[0].displayName : "N/A";
+        let location = d.sourceLocation || "Unknown";
+        let occuranceTime = d.flrID ? d.flrID.slice(0, 10) : "Unknown";
+        let activeRegion = d.activeRegionNum || null;
+        let linkedEvents = Array.isArray(d.linkedEvents)
+            ? d.linkedEvents.map(event => event.activityID)
+            : null;
+
+        result.push({
+            beginTime,
+            peakTime,
+            endTime,
+            intensity,
+            magnitude,
+            instrumentUsed,
+            location,
+            occuranceTime,
+            activeRegion,
+            linkedEvents
+        });
+    }
+    return result;
+}
+
+function milliSecConverter(mls) {
+    let totalSeconds = (mls / 1000).toFixed(1);
+    let totalMinutes = (mls / (1000 * 60)).toFixed(1);
+    let totalHours = (mls / (1000 * 60 * 60)).toFixed(1);
+    let totalDays = (mls / (1000 * 60 * 60 * 24)).toFixed(1);
+
+    if (mls < 60000) {
+        return totalSeconds + ' Sec';
+    } else if (mls < 3600000) {
+        return totalMinutes + ' Min';
+    } else if (mls < 216000000) {
+        return totalHours + ' Hrs'
+    } else {
+        return totalDays + ' days'
+    }
 }
 
 
